ace31c98e44c7a03bc59feec74df02e2b53d812f
boonloong <obl0702@gmail.com>
2022-11-03 20:26:34 +0800

    Adding PetTypeService, Closes #19

---
+++ pet-clinic-data/src/main/java/com/ongbl/sfgpetclinic/map/PetTypeMapService.java
@@ -0,0 +1,44 @@
+/*
+ * Created by boonloong
+ */
+
+package com.ongbl.sfgpetclinic.map;
+
+import com.ongbl.sfgpetclinic.model.Pet;
+import com.ongbl.sfgpetclinic.model.PetType;
+import com.ongbl.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+import java.util.Set;
+
+/**
+ * @created: 3/11/2022
+ * @author: boonloong
+ */
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/ongbl/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,14 @@
+/*
+ * Created by boonloong
+ */
+
+package com.ongbl.sfgpetclinic.services;
+
+import com.ongbl.sfgpetclinic.model.PetType;
+
+/**
+ * @created: 3/11/2022
+ * @author: boonloong
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
