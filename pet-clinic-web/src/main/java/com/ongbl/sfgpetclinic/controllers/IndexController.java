5864e10a85c26612c14a3c89fc8cc1a774d8547c
boonloong <obl0702@gmail.com>
2022-11-03 22:36:54 +0800

    Fixed Broken Links. Closes #36

--- pet-clinic-web/src/main/java/com/ongbl/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/ongbl/sfgpetclinic/controllers/IndexController.java
@@ -17,4 +17,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping({"/oups"})
+    public String oupsHandler(){
+        return "notimplemented";
+    }
 }
